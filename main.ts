30953dc9812686421b9e0b83729c0c5c5f02d498
yano3nora <yano3@alhino.jp>
2024-10-05 11:43:29 +0900

    fix bug

--- main.ts
+++ main.ts
@@ -6,24 +6,34 @@ const paths = Deno.args.filter((s) => !s.startsWith('--'))
 const args = Deno.args.filter((s) => s.startsWith('--'))
 const src = paths.at(0)
 const dest = paths.at(1)
+const srcInfo = await (async () => {
+  try {
+    return await Deno.stat(src || '')
+  } catch {
+    return undefined
+  }
+})()
 
-if (
-  !src ||
-  !(await Deno.stat(src)).isFile
-) {
+if (!src || !srcInfo?.isFile) {
   console.error('No path/to/file.xlsx was given.')
   Deno.exit(1)
 }
 
 excel2json(src, args).then(async (json) => {
   const destPath = dest || src.replace(extname(src), '.json')
-  const destInfo = await Deno.stat(destPath)
+  const destInfo = await (async () => {
+    try {
+      return await Deno.stat(destPath)
+    } catch {
+      return undefined
+    }
+  })()
 
   if (
     args.includes('--no-override') &&
     (
-      destInfo.isFile ||
-      destInfo.isDirectory
+      destInfo?.isFile ||
+      destInfo?.isDirectory
     )
   ) {
     console.error('Abort override by arg of --no-override.')
